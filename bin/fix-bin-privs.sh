24583718fd7ee9b00d5283dfa33c038b87f9702b
extaz-site <site@extazshop.ru>
2016-02-12 09:42:09 +0300

    fix symlinks too (entry in bin/ can be a symlink to real script outside)

--- bin/fix-bin-privs.sh
+++ bin/fix-bin-privs.sh
@@ -2,5 +2,5 @@
 
 # find all bin/ folders inside home dir and make executable all files there
 find $HOME/ -type d -name bin | while read bin ; do
-    find "$bin" -type f | xargs chmod -v +x
+    find "$bin" -type f -or -type l | xargs chmod -v +x
 done
