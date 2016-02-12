543f99f004bc5e09454979f48ae0b4f8803d4e61
extaz-site <site@extazshop.ru>
2016-02-12 09:36:10 +0300

    script for fixing bin privs

---
+++ bin/fix-bin-privs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# find all bin/ folders inside home dir and make executable all files there
+find $HOME/ -type d -name bin | while read bin ; do
+    find "$bin" -type f | xargs chmod -v +x
+done
