ab77494cc998b4ccd357eadf5b4f1828eb0ee33b
Cronfy <cronfy@gmail.com>
2016-02-29 18:49:53 +0300

    useful functions

---
+++ bin/get-mysql-args.php
@@ -0,0 +1,16 @@
+<?php
+
+ini_set('display_errors', true);
+error_reporting(-1);
+
+function error($no, $str) {
+    fwrite(STDERR, "$str\n");
+    die(1);
+}
+    
+//set_error_handler("error"); 
+
+$config = $argv[1];
+include "$config"; 
+echo "-u $config_db_user -p$config_db_password $config_db_name";
+
