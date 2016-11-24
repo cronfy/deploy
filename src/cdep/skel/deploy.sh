05c3a0cf450b6bff8997d6e781ae98274c589cdc
Cronfy <cronfy@gmail.com>
2016-11-24 16:07:48 +0300

    added ioncube test

--- src/cdep/skel/deploy.sh
+++ src/cdep/skel/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+php -m | grep -qi ioncube && {
+	echo "ERROR: ionCube is incompatible with deployer. Please disable ionCube."
+	exit 1
+}
+
 cd "`dirname $0`/config"
 
 dep deploy "$@"
